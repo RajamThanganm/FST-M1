56b376582718eee4d0c3affa9a44a4ecf999b41f
RajamThanganm <101310837+RajamThanganm@users.noreply.github.com>
2022-03-28 16:54:28 +0530

    Add files via upload

---
+++ SQL/Activities/Activity5.sql
@@ -0,0 +1,10 @@
+
+select * from salesman;
+
+update salesman set grade = 200 where salesman_city = 'Rome';
+
+update salesman set grade = 300 where salesman_name = 'James Hoog';
+
+update salesman set salesman_name = 'Pierre' where salesman_name = 'McLyon';
+
+select * from salesman;
