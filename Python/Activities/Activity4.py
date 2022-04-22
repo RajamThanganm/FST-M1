b1f0ab98f288dfc61d67d3e0a5b4315a70e9ddce
RajamThanganm <101310837+RajamThanganm@users.noreply.github.com>
2022-04-22 10:42:48 +0530

    Add files via upload

---
+++ Python/Activities/Activity4.py
@@ -0,0 +1,45 @@
+"""
+
+    Make a two-player Rock-Paper-Scissors game.
+    Remember the rules:
+        Rock beats scissors
+        Scissors beats paper
+        Paper beats rock
+
+"""
+
+while True:
+    print("start")
+    player1 = input("enter player 1 name")
+    player2 = input("enter player 2 name")
+
+    player1_choice = input("choose rock,paper or scissors for player1")
+    player2_choice = input("choose rock,paper or scissors for player2")
+
+    player1_choice_lower = str(player1_choice).lower()
+    print(player1_choice_lower)
+    player2_choice_lower = str(player2_choice).lower()
+    print(player2_choice_lower)
+
+    if player1_choice_lower == player2_choice_lower:
+       print ("Its  a tie, Repeat the game")
+    elif player1_choice_lower == "rock":
+        if player2_choice_lower == "paper":
+           print("Paper beats rock") 
+        elif player2_choice_lower == "scissors":
+           print("Rock beats scissors")
+    elif player1_choice_lower == "paper":
+        if player2_choice_lower == "rock":
+           print("Paper beats rock") 
+        elif player2_choice_lower == "scissors":
+           print("Scissors beats paper")
+    elif player1_choice_lower == "scissors":
+        if player2_choice_lower == "rock":
+          print("Rock beats Scissor")
+        elif player2_choice_lower == "paper":
+          print("Scissors beats paper")
+    else:
+        print("end of game")
+    playagain = input("do you want to play the game?").lower()
+    if playagain!="yes": 
+       raise SystemExit
---
+++ Python/Activities/Activity5.py
@@ -0,0 +1,4 @@
+j = int(input("enter the number for multiplication tablw"))
+for i in range(1, 11):
+    text = str(i) + "X" + str(j) + "=" + str(i*j)
+    print(text)
